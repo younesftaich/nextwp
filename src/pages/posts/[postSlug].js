99bf3ad99e203e175d6c3cdba9e30976f98ede8d
younesftaich <76970345+younesftaich@users.noreply.github.com>
2022-04-02 22:37:40 +0800

    Update [postSlug].js

--- src/pages/posts/[postSlug].js
+++ src/pages/posts/[postSlug].js
@@ -10,12 +10,25 @@ import styles from '../../styles/Home.module.css'
 export default function Post({ post, site }) {
   const [currentqst, setQuestion] = useState(0)
   const [totalqst, setTotalQst] = useState(0)
+  const [question, setQst] = useState("")
+  const [answers, setAnswers] = useState([])
+  const [answerss, setAnswerss] = useState(["ok","ok"])
+  const [showanswers, setShowAnswers] = useState(false)
 
   const nextqst = () => {
     let totalqst = Object.keys(post.quiz.questions[0].question).length
     setTotalQst(totalqst)
-    console.log(totalqst)
-    console.log(post.quiz.questions[0].question[currentqst])
+    if (post.quiz.questions[0].question[currentqst]) {
+     // console.log(post.quiz.questions[0].question[currentqst].answers)
+      setQst(post.quiz.questions[0].question[currentqst].mainQuestion)
+      setShowAnswers(true)
+      setAnswers(post.quiz.questions[0].question[currentqst].answers)
+
+  
+      
+    }
+    else 
+    console.log ("salina")
     setQuestion(currentqst+1)
 
   }
@@ -46,6 +59,26 @@ export default function Post({ post, site }) {
     />
 
     <button onClick={ () => nextqst() }>Click me</button>
+
+    <h1>
+      {question}
+    </h1>
+
+{
+  Object.keys(answers).map((answernum, index) => {
+    let answerTitle = answers[answernum].answerTitle
+    let correct = answers[answernum].correct
+    
+    return (
+        <p key={index}>
+        {answerTitle}
+            {correct}
+        </p>
+    );
+})
+}
+
+
 
      
 
